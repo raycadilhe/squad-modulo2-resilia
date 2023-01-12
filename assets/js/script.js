7dd515ca3532af28f406a00b358822d7469f8b7c
PedroNicacio <117321352+PedroNicacio@users.noreply.github.com>
2023-01-12 18:47:07 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,25 @@ const leftSide = document.getElementById("leftSide");
 const rightSide = document.getElementById("rightSide");
 
 frontSide.addEventListener('click', () => {
-  frontSide.style.display = "none";
-  leftSide.style.display = "block";
-  rightSide.style.display = "block";
-  square.style.transform = "rotate3d(0,0,0,90deg)";
-  document.title = "Login";
+    frontSide.style.display = "none";
+    leftSide.style.display = "block";
+    rightSide.style.display = "block";
+    square.style.transform = "rotate3d(0,0,0,90deg)";
+    document.title = "Login";
 });
 
 rightSide.addEventListener('click', () => {
-  frontSide.style.display = "block";
-  leftSide.style.display = "none";
-  rightSide.style.display = "block";
-  square.style.transform = "rotate3d(0,-1,0,90deg)";
-  document.title = "Cadastro";
+    frontSide.style.display = "block";
+    leftSide.style.display = "none";
+    rightSide.style.display = "block";
+    square.style.transform = "rotate3d(0,-1,0,90deg)";
+    document.title = "Cadastro";
 });
 
 leftSide.addEventListener('click', () => {
-  frontSide.style.display = "block";
-  leftSide.style.display = "block";
-  rightSide.style.display = "none";
-  square.style.transform = "rotate3d(0,1,0,90deg)";
-  document.title = "Recuperação de Conta";
+    frontSide.style.display = "block";
+    leftSide.style.display = "block";
+    rightSide.style.display = "none";
+    square.style.transform = "rotate3d(0,1,0,90deg)";
+    document.title = "Recuperação de Conta";
 });
