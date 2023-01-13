92dd86af98600e7771e27f9fd8d717b27ffa6201
raycadilhe <62357420+raycadilhe@users.noreply.github.com>
2023-01-13 14:35:06 -0300

    atualização js

---
+++ assets/js/indexScript.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector('.submit');
+
+botao.addEventListener('click', sucesso, true);
+
+function sucesso () {
+    window.alert('Em breve você receberá todas as informações sobre a ferramenta em seu e-mail!')
+};
